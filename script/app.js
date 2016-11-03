df18e4b3996e6fdeabacee4a287a647c75b89893
simon <simon.tanner28@gmail.com>
2016-11-03 15:50:28 +0000

    exercise2

--- script/app.js
+++ script/app.js
@@ -1,1 +1,27 @@
-/* Write your code here! */
+
+// $('img').click(function(e){
+//     $('div.overlayHidden').toggleClass('overlay');
+//     $(this).addClass('clickedImg');
+// }); 
+
+
+
+var img,
+	alt;
+$(function() {
+	$('img').click(function() {
+		$('div.overlayHidden').addClass('overlay');
+		
+		img = $(this).attr('src');
+		alt = $(this).attr('alt');
+		console.log(img);
+		
+		$('.overlay img').attr('src', img);
+		$('.image-caption').text(alt);
+		
+	})
+	
+	$('.overlayHidden').click(function() {
+			$(this).removeClass('overlay');
+		})
+});
