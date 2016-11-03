a31af644bf51f4a30505b4c59f9ac99e5fdf2cb5
simon <simon.tanner28@gmail.com>
2016-11-03 18:49:07 +0000

    exercise3

--- script/app.js
+++ script/app.js
@@ -1,1 +1,50 @@
 /* Write your code here! */
+
+  
+//   var mq = window.matchMedia( "(min-width: 641px)" );
+  
+//   if(mq.matches) {
+//         var sticky = $("form").sticky({topSpacing:0});
+//         sticky();
+//   }
+//   else {
+//       $("form").removeClass('sticky');
+//       $("window").on('scroll', function(){("text").css('width','100%')});
+//   }
+
+
+  
+  
+  //callback
+  
+  if (matchMedia) {
+  var mq = window.matchMedia("(min-width: 500px)");
+  mq.addListener(WidthChange);
+  WidthChange(mq);
+}
+
+
+function WidthChange(mq) {
+  if (mq.matches) {
+    // window width is at least 500px
+    $("form").sticky({topSpacing:0});
+    $(".container").css('flex-direction','row');
+  } else {
+    // window width is less than 500px
+    $("form").unstick();
+    $(".container").css('flex-direction','column');
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
