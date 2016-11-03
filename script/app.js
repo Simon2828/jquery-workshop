b5d1168b856fc6d1756faeb8c78c994d7f8948d9
simon <simon.tanner28@gmail.com>
2016-11-03 20:56:16 +0000

    exercise4

--- script/app.js
+++ script/app.js
@@ -1,27 +1,38 @@
-
-// $('img').click(function(e){
-//     $('div.overlayHidden').toggleClass('overlay');
-//     $(this).addClass('clickedImg');
-// }); 
-
-
-
-var img,
-	alt;
-$(function() {
-	$('img').click(function() {
-		$('div.overlayHidden').addClass('overlay');
-		
-		img = $(this).attr('src');
-		alt = $(this).attr('alt');
-		console.log(img);
-		
-		$('.overlay img').attr('src', img);
-		$('.image-caption').text(alt);
-		
-	})
+$('#signup-form').on('submit', function(event){
+	var password = $('input[name=password]').val();
+	var password2 = $('input[name=password2]').val();
+	if (password !== password2) {
+		alert('Your passwords do not match');
+	}
+	else if (password.length<10) {
+		alert('Your password is too short');
+	}
+	else {
+		return;
+	}
+	event.preventDefault();
 	
-	$('.overlayHidden').click(function() {
-			$(this).removeClass('overlay');
-		})
 });
+
+
+
+
+// Make the following validation on the fields:
+// The password should be at least 10 characters
+// The password and confirm password should be the same
+// If the validation passes, then let the form submit normally by NOT calling preventDefault.
+// If the validation fails, prevent the form from submitting and display an alert to the user telling them what went wrong
+
+
+
+
+// if (password.length>=10 && password === password2) {
+// 	$('#signup-form').on('submit', function(event) {
+// });
+// }
+// else {
+// 	$('#signup-form').on('submit', function(event) {
+//     event.preventDefault();
+// 	});
+//     alert('your password is not good enough');
+// }
